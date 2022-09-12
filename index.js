e51daf66ae3a3ace12ad334baf66dd18710d7ff6
Hubert Rutkowski <hubr2000@gmail.com>
2022-09-13 01:01:03 +0200

    login with password

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
 import usersRouter from './routes/users-routes.js'
+import authRouter from './routes/auth-routes.js'
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use('/', express.static(join(__dirname, 'public')));
 app.use('/api/users',usersRouter);
+app.use('/api/auth',authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,3 +1,25 @@
 import express from 'express';
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import{jwtTokens} from '../utils/jwt-helpers.js';
+
+const router = express.Router();
+
+
+router.post('/login', async (req,res) => {
+    try {
+        const {email,password} = req.body;
+        const users = await pool.query('SELECT * FROM users WHERE user_email = $1', [email]);
+        if(users.rows.length === 0) return res.status(401).json({error : "Email is incorrect"})
+        //password check
+        const validPassword = await bcrypt.compare(password, users.rows[0].user_password);
+        if(!validPassword) return res.status(401).json({error: "Incorrect password"});
+        //JWT
+    } catch (error) {
+
+    }
+})
+
+
+export default router;
